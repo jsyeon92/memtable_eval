7cd3dabe0b4c3fdc42db5b7441b23d84a32879de
root <root@ip-172-31-31-209.us-west-2.compute.internal>
2018-04-23 15:14:47 +0000

    range_16G

---
+++ range_16G/3_all_plot.py
@@ -0,0 +1,35 @@
+DATA_DIR=./memtable_eval/data
+
+#cd $DATA_DIR
+
+bufflist="64 16384"
+factors="
+	memtable.hit 
+	memtable.miss
+	l0.hit
+	l1.hit
+	block.cache.hit
+	block.cache.miss
+"
+
+for buff in $bufflist; do
+	for factor in $factors; do
+		fname=$factor.$buff.dat
+		echo "$fname ...."
+		python2.7 ./stat_plot.py $fname
+	done
+done
+
+factors="
+	fillrandom
+	seekrandom
+"
+for buff in $bufflist; do
+	for factor in $factors; do
+		fname=$factor.$buff.dat
+		echo "$fname ...."
+		python2.7 ./perf_plot.py $fname
+	done
+done
+
+
